884b74c467b7ff1adf2763b26b9f07f75f2377f3
splinterific <markgpreston@gmail.com>
2014-06-12 23:50:29 +0100

    updated
    added full file extension testing

--- script1.py
+++ script1.py
@@ -1,7 +1,17 @@
 # standard file import and export with known file names and delimiter is comma (default)
 
 import csv
-inputfile = raw_input("What's the file called?")
+inputfile = raw_input("What's the file called?") 
+
+
+""" 
+using this allows the file to be picked up 
+from the same location as the script.py.
+By introducting a full file location i can simply call from that location, 
+(e.g. inputfile = ("c:\\test\\test.csv")) but I need to be sure to include the escape characters 
+"""
+
+
 output = raw_input("What will we call the file?(include extension)")
 
 def known():
